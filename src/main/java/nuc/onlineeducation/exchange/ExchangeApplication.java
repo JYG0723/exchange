5c4ab6bb9dbf313ef679e84712427f204f051a94
hikaricp <jiyongguang0723@gmail.com>
2018-01-05 22:08:11 +0800

    exchange init

---
+++ src/main/java/nuc/onlineeducation/exchange/ExchangeApplication.java
@@ -0,0 +1,12 @@
+package nuc.onlineeducation.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeApplication.class, args);
+    }
+}
